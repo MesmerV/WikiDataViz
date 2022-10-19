205a307d788f20bb3aae572daafc7a5661b84ebb
MesmerV <victor.pccb@gmail.com>
2022-10-19 23:02:26 +0200

    add gitignore and restructured data

--- static/js/script.js
+++ static/js/script.js
@@ -2,20 +2,43 @@ const ctx = {
     GLYPH_SIZE: 16,
     w: 820,
     h: 720,
-    data: {},
+    data: [],
+    top_pages_count: 5,
 };
 
-function initSVGcanvas(){
-    console.log(ctx.data);
+function initMainView(svgEl){
+    
+    //console.log(ctx.data);
+
+    topPages = {}
+
+    //transform data
+
+    Object.entries(ctx.data).forEach(function(day,i){
+        let rankDate = day[0];
+        day[1].forEach(function(page,j){
+            if(page.rank < ctx.top_pages_count ){
+                if (!topPages.hasOwnProperty(page.article)){
+                    topPages[page.article] = [];
+                }
+                
+                topPages[page.article].push({"date":rankDate, "views":page.views, "rank":page.rank});
+            }
+        });    
+    });
+
+    //console.log(topPages);
+    
+    //draw first viZ
 };
 
 
-function loadData(){
+function loadData(svgEl){
     d3.json("/static/data/top_pageviews.json")
         .then(function(rawdata){
             // store data as constant
             ctx.data = rawdata;
-            initSVGcanvas();
+            initMainView(svgEl);
         })
         .catch(function(error){console.log(error)});
 };
@@ -25,11 +48,12 @@ function createViz(){
     var svgEl = d3.select("#main").append("svg");
     svgEl.attr("width", ctx.w);
     svgEl.attr("height", ctx.h);
+
     var rootG = svgEl.append("g").attr("id", "rootG");
 
     // group for background elements (axes, labels)
     rootG.append("g").attr("id", "bkgG");
-    loadData();
+    loadData(svgEl);
 
 
 
