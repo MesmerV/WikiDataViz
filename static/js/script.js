f70db44e08cf0fab88b726c256f5b0e1e38fc9da
MesmerV <victor.pccb@gmail.com>
2022-11-11 09:55:12 +0100

    new animated Time Series implementation

--- static/js/script.js
+++ static/js/script.js
@@ -2,35 +2,48 @@ const ctx = {
     GLYPH_SIZE: 16,
     w: 820,
     h: 720,
+
+
+    //timeSeries constants
     graph_h: 700, 
     graph_w: 800,
+    num_graph: 25,
     timeAxisHeight: 20,
+    maxViews:0,
+    minViews:0,
     hmargin: 10,
+    margin: {
+        top: 20,
+        right: 30,
+        bottom: 30,
+        left: 40
+        },
     data: [],
     timeParser: d3.timeParse("%m%d"),
     top_pages_count: 20,
 };
 
-// Temporary way to get data
+// Temporary way to get data 
+
+// TODO filter --> if(name=="Main_Page" || name=="Special:Search")return;
+// TODO remove max_pages from page construction and make it draw page wi give (lot cleaner...) 
 
 function getTopPages(data){
 
     //topPages element that we can feed into the time Series
     var topPages = {};
-    topPages.maxViews = 0;
-    topPages.minViews = 0;
     
     //transform data
     //TODO : not the best way to filter the data
-    Object.entries(ctx.data).forEach(function(day,i){
+    Object.entries(data).forEach(function(day,i){
         let rankDate = day[0];
         day[1].forEach(function(page,j){
-            if(page.rank < ctx.top_pages_count ){
+            if(page.rank < ctx.top_pages_count){
                 if (!topPages.hasOwnProperty(page.article)){
                     topPages[page.article] = [];
                 }
-                if(page.views > topPages.maxViews)topPages.maxViews = page.views;
-                if(page.views < topPages.minViews)topPages.minViews = page.views;
+                if(page.views > ctx.maxViews)ctx.maxViews = page.views;
+                if(page.views < ctx.minViews)ctx.minViews = page.views;
 
                 topPages[page.article].push({"date":rankDate, "views": page.views, "rank":page.rank});
             }
@@ -42,7 +55,7 @@ function getTopPages(data){
     
 }
 
-//change to cleaner one 
+// DEPRECATED 
 function initMainView(svgEl, topPages){
     
     //console.log(ctx.data);
@@ -50,11 +63,11 @@ function initMainView(svgEl, topPages){
     
     //set views scale
     ctx.viewsScale = d3.scaleLog()
-                  .domain([10000+ topPages.minViews,topPages.maxViews])
+                  .domain([10000+ ctx.minViews,ctx.maxViews])
                   .range([1,ctx.graph_h]);
     
                   
-    console.log(ctx.viewsScale(topPages.maxViews));
+    console.log(ctx.viewsScale(ctx.maxViews));
 
     
     //y axis
@@ -72,20 +85,18 @@ function initMainView(svgEl, topPages){
                `translate(${ctx.hmargin},${ctx.graph_h-ctx.timeAxisHeight})`)
          .call(d3.axisBottom(timeScale).ticks(d3.timeMonth.every(1)));
                       
-    let num_graph = 35;
-
+    
     //draw first viZ
     Object.entries(topPages).forEach(function(page,i){
         
-        if(i >= num_graph)return;
+        if(i >= ctx.num_graph)return;
         
 
         let name = page[0];
         let days = page[1];
         console.log(name,days)
      
-        if(name=="Main_Page" || name=="Special:Search")return;
-    
+        
         
 
         // add tooltip, see https://www.d3-graph-gallery.com/graph/interactivity_tooltip.html
@@ -128,8 +139,8 @@ function initMainView(svgEl, topPages){
             .attr("stroke", "#69b3a2")
             .attr("stroke-width", 1.5)
             .attr("d", d3.line()
-              .x(function(d) { return timeScale(ctx.timeParser(d.date)) })
-              .y(function(d) { return (ctx.graph_h - ctx.viewsScale(d.views)) })
+              .x( d => timeScale(ctx.timeParser(d.date)) )
+              .y( d =>  (ctx.graph_h - ctx.viewsScale(d.views)) )
             )
             .on("mouseover", mouseover)
             .on("mousemove", mousemove)
@@ -141,62 +152,126 @@ function initMainView(svgEl, topPages){
 };
 
 //better structured TimeSeries with animation flexibility
-function AnimatedTimeSeries(){
-    const svg = d3.create("svg")
-        .attr("width", width)
-        .attr("height", height)
-        .attr("viewBox", [0, 0, width, height])
+
+function AnimatedTimeSeries(svg_TS, topPages){
+    //creates graph on svg and assign an update method to it
+
+
+    //var svg = d3.select("#main").append("svg")//d3.create("svg")
+    svg_TS.attr("width", ctx.graph_w)
+        .attr("height", ctx.graph_h)
+        .attr("viewBox", [0, 0, ctx.graph_w, ctx.graph_h])
         .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
-  
-    const zx = x.copy(); // x, but with a new domain.
-  
+    
+    // init scales
+    const yScale = d3.scaleLog()
+    .domain([10000 + ctx.minViews,ctx.maxViews])
+    .range([ctx.graph_h - ctx.margin.bottom, ctx.margin.top]);
+
+    const timeScale = d3.scaleTime()
+        .domain(d3.extent(Object.keys(ctx.data), (d) => ctx.timeParser(d)))
+        .rangeRound([ctx.margin.left,ctx.graph_w - ctx.margin.right]);
+
+    //init axis 
+    xAxis = (g, scale = timeScale) => g
+    .attr("transform", `translate(0,${ctx.graph_h - ctx.margin.bottom})`)
+    .call(d3.axisBottom(scale).ticks(ctx.graph_w / 80).tickSizeOuter(0))
+
+    yAxis = (g, scale = yScale) => g
+    .attr("transform", `translate(${ctx.margin.left},0)`)
+    .call(d3.axisLeft(scale).ticks(ctx.graph_h / 40))
+    .call(g => g.select(".domain").remove())
+
+    //line function
     const line = d3.line()
-        .x(d => zx(d.date))
-        .y(d => y(d.close));
+    .x( d => timeScale(ctx.timeParser(d.date)) )
+    .y( d =>  (yScale(d.views)) );
+
+    // draw every paths
+    const paths = []
+
+    Object.entries(topPages).forEach(function(page,i){
+        
+        //limit the number of graph drawn
+        if(i >= ctx.num_graph)return;
+        
+        let days = page[1];
+        paths.push(
+            svg_TS.append("path")
+            .attr("fill", "none")
+            .attr("stroke", "steelblue")
+            .attr("stroke-width", 3)
+            .attr("stroke-miterlimit", 2)
+            .attr("d", line(days))
+            );
+    })
+    
+    // append axes to graph 
+    const gx = svg_TS.append("g")
+        .call(xAxis, timeScale);
   
-    const path = svg.append("path")
-        .attr("fill", "none")
-        .attr("stroke", "steelblue")
-        .attr("stroke-width", 1.5)
-        .attr("stroke-miterlimit", 1)
-        .attr("d", line(data));
+    const gy = svg_TS.append("g")
+        .call(yAxis, yScale);
   
-    const gx = svg.append("g")
-        .call(xAxis, zx);
-  
-    const gy = svg.append("g")
-        .call(yAxis, y);
-  
-    return Object.assign(svg.node(), {
-      update(domain) {
-        const t = svg.transition().duration(750);
-        zx.domain(domain);
-        gx.transition(t).call(xAxis, zx);
-        path.transition(t).attr("d", line(data));
+    return Object.assign(svg_TS, {
+        // function that animates the graph
+        update(domain,duration) {
+        const t = svg_TS.transition()
+                .duration(duration)
+                .ease(d3.easeLinear); //make the transition linear
+        timeScale.domain(domain);
+        gx.transition(t).call(xAxis, timeScale);
+        Object.entries(topPages).forEach(function(page,i){
+            let days = page[1];
+            if(i<ctx.num_graph)paths[i].transition(t).attr("d", line(days));
+        })
       }
     });
   }
 
-function loadData(svgEl){
-    d3.json("/static/data/top_pageviews.json")
+
+
+async function createViz(){
+    console.log("Using D3 v"+d3.version);
+
+
+    //create svg Elements
+
+    // old viZ
+        //var svgEl = d3.select("#main").append("svg");
+        //svgEl.attr("width", ctx.w);
+        //svgEl.attr("height", ctx.h);
+        //initMainView(svgEl,topPages);
+    
+
+    //create and add svgElement to page
+    var svg_TS = d3.select("#main").append("svg");
+
+
+    //Load TS Data and feed it to graphs
+    d3.json("/static/data/top_pageviews.json") // returns a promise : asynchonous
         .then(function(rawdata){
-            // store data as constant
+            // store data as constant of the page
             ctx.data = rawdata;
+            
+            //get TopPages object needed for TimeSeries
             topPages = getTopPages(ctx.data);
-            initMainView(svgEl,topPages);
+
+            //create TS and affect it a timeFrame
+            timeframe = [new Date("1899-08-01"), new Date("1899-12-30")];
+
+            AnimatedTimeSeries(svg_TS,topPages)
+            svg_TS.update(timeframe);
+
+            timeframe = [new Date("1900-04-01"), new Date("1900-08-01")];
+            
+
+
+            
         })
         .catch(function(error){console.log(error)});
-};
 
-
-
-function createViz(){
-    console.log("Using D3 v"+d3.version);
-    var svgEl = d3.select("#main").append("svg");
-    svgEl.attr("width", ctx.w);
-    svgEl.attr("height", ctx.h);
-
-    // group for background elements (axes, labels)
-    loadData(svgEl);
+        await new Promise(r => setTimeout(r, 1000));
+        svg_TS.update(timeframe,5000);
 
 };
